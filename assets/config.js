9ffbcd73bd0f7f6dd6a0820f8e2c24515ed080a5
ElCaliente69 <144935983+ElCaliente69@users.noreply.github.com>
2026-07-24 11:42:07 +0200

    Activar subida de fotografías a Drive

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,3 @@
 window.HOT_HOST_CONFIG = Object.freeze({
-  // Paste the deployed Google Apps Script Web App URL ending in /exec.
-  driveUploadEndpoint: ""
+  driveUploadEndpoint: "https://script.google.com/macros/s/AKfycbytLhgU__o7swaSgg0kJy429VLfH_kiUbGHIroO5MiPFY7OyLuIHYqYA-zOanl9cHSX1g/exec"
 });
